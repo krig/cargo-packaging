60df8d512f0a56ac4dfe4dc6b6895a1bf629f457
Kristoffer Grönlund <krig@koru.se>
2016-06-23 10:23:59 +0200

    fix version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='cargoapi',
-      version='0.0.1',
+      version='0.1.0',
       description='Tools for interacting with the cargo package manager',
       author='Kristoffer Gronlund',
       author_email='kgronlund@suse.com',
